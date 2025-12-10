1cbb5bdabe7f14d48c80c0268af2119b63f9cc22
Bara-Gherman Octav <octav.bara@stud.ubbcluj.ro>
2025-12-10 19:43:47 +0200

    FOR ORGANIZERS: Import Partricipants + Participant CRUD

--- backend/src/main/java/com/example/backend/dto/CreateParticipantRequest.java
+++ backend/src/main/java/com/example/backend/dto/CreateParticipantRequest.java
@@ -1,12 +1,12 @@
+// com.example.backend.dto.CreateParticipantRequest
 package com.example.backend.dto;
 
 import lombok.Data;
-
-//to be implemented later
 
 @Data
 public class CreateParticipantRequest {
     private String name;
     private String email;
+    private Boolean attending;
     private Long eventId;
 }
